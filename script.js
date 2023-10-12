3c4d3ded668743e2d0304178613fdac3632ce3c1
somu878 <somumydhili19@gmail.com>
2023-10-12 19:48:32 +0530

    added rulebtns

--- script.js
+++ script.js
@@ -1,35 +1,17 @@
 const buttons = document.querySelectorAll('.btns');
-const options = ["rock", "paper", "scissors"];
-const wl =document.querySelector('.wl')
-
-buttons.forEach(element => {
-    element.addEventListener('click', () => {
-        const player = element.innerText.toLowerCase();
-        const computer = options[Math.floor(Math.random() * options.length)];
-        rockPaperScissor(player,computer);
-        document.querySelector('.player').innerText = `The player: ${player}`;
-        document.querySelector('.computer').innerText=`The computer:${computer}`
-        
-       
-    });
-   
+const container = document.querySelector('.container');
+const rules = document.querySelector('.ruleli')
+const xbtn = document.querySelector('.xbtn')
+const rulebtn = document.querySelector('.rulebtn')
+rules.style.display ='none';
+rulebtn.addEventListener('click',()=>{
+    rules.style.display ='block';
+})
+xbtn.addEventListener("click", () => {
+    rules.style.display ='none';
 });
-function rockPaperScissor(player,computer) {
-    
-    if (player === computer) {
-        // window,alert();
-        wl.innerText="It's a tie!";
-    } else if (
-        (player === "rock" && computer === "scissors") ||
-        (player === "paper" && computer === "rock") ||
-        (player === "scissors" && computer === "paper")
-    ) {
-        // window,alert(`You win! Computer chose ${computer}.`);
-        wl.innerText=`You win! Computer chose ${computer}.`;
-        
-    } else {
-        // window,alert(`You lose! Computer chose ${computer}.`);
-        wl.innerText=`You lose! Computer chose ${computer}.`;
-    }
-    
-}
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        container.style.display = 'none';
+    });
+});
