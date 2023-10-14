e54488bc4dc0eb99e7584e655127f4cd44cb97f6
somu878 <somumydhili19@gmail.com>
2023-10-14 16:03:22 +0530

    added win animation

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const container = document.querySelector('.btn');
 const rules = document.querySelector('.ruleli')
 const xbtn = document.querySelector('.xbtn')
 const rulebtn = document.querySelector('.rulebtn')
+const play =document.querySelector('.play')
+const con = document.querySelector('.con')
+const compchose =document.querySelector('.compchose')
 rules.style.display ='none';
 rulebtn.addEventListener('click',()=>{
     rules.style.display ='block';
@@ -15,4 +18,13 @@ buttons.forEach(button => {
         container.style.display = 'none';
     });
 });
+
+
+play.addEventListener('click', () => {
+    con.style.left = '600px';
+    compchose.style.backgroundImage = `url('/icons/icons8-fist-67\ 1.png')`;
+    compchose.style.backgroundSize = "90px"
+    compchose.style.borderColor = "#FFA943"
+});
+
 
